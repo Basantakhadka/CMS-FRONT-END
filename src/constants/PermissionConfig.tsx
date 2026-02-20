ec552ca69045730b156eaa35ff1d51e812480250
Basantakhadka <basantakhadka963@gmail.com>
2026-02-20 12:53:18 +0545

    root level permission implemented

---
+++ src/constants/PermissionConfig.tsx
@@ -0,0 +1,38 @@
+// DASHBOARD
+export const DASHBOARD = 'dashboard';
+export const DASHBOARD_CONTRACTS = 'dashboard:contracts';
+export const DASHBOARD_ALERTS = 'dashboard:alerts';
+
+// CONTRACTS
+export const CONTRACTS = 'contracts';
+export const CONTRACTS_SEARCH = 'contracts:search';
+export const CONTRACTS_LIST = 'contracts:list';
+export const CONTRACTS_SUMMARY = 'contracts:summary';
+export const CONTRACTS_TABLE = 'contracts:table';
+export const CONTRACTS_ADD = 'contracts:add';
+export const CONTRACTS_DELETE = 'contracts:delete';
+export const CONTRACTS_UPDATE = 'contracts:update';
+export const CONTRACTS_VIEW = 'contracts:view';
+
+// ALERTS
+export const ALERTS = 'alerts';
+export const ALERTS_LIST = 'alerts::list';
+export const ALERTS_ADD = 'alerts::add';
+export const ALERTS_UPDATE = 'alerts::update';
+export const ALERTS_DELETE = 'alerts::delete';
+export const ALERTS_EDIT = 'alerts::edit';
+
+// IAM
+export const IAM = 'iam';
+export const IAM_GENERAL_PASSWORDPOLICY = 'iam:general:passwordPolicy';
+export const IAM_GENERAL_MULTIFACTORAUTHENTICATION = 'iam:general:multiFactorAuthentication';
+export const IAM_USERS_LIST = 'iam:users:list';
+export const IAM_USERS_ADD = 'iam:users:add';
+export const IAM_USERS_UPDATE = 'iam:users:update';
+export const IAM_USERS_EDIT = 'iam:users:edit';
+export const IAM_ROLES_LIST = 'iam:roles:list';
+export const IAM_ROLES_ADD = 'iam:roles:add';
+export const IAM_ROLES_UPDATE = 'iam:roles:update';
+export const IAM_ROLES_DELETE = 'iam:roles:delete';
+export const IAM_ROLES_EDIT = 'iam:roles:edit';
+export const IAM_USERS_DELETE = 'iam:users:delete';
